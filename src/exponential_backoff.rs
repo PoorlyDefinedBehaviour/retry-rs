ba4f202065f549a83f49e32e46515e74cf8ff67d
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2022-03-30 20:37:04 -0300

    fix: exponential backoff algo

--- src/exponential_backoff.rs
+++ src/exponential_backoff.rs
@@ -3,26 +3,15 @@ use std::time::Duration;
 use async_trait::async_trait;
 
 struct ExponentialBackoff {
-  pub exponent: u32,
-  pub wait_for: u32,
-}
-
-impl ExponentialBackoff {
-  /// The backoff time will be start^2.
-  pub fn binary(wait_for: u32) -> Self {
-    assert!(wait_for > 1, "initial waiting time must be greater than 1");
-    Self {
-      wait_for,
-      exponent: 2,
-    }
-  }
+  pub max: u32,
+  pub start: u32,
 }
 
 #[async_trait]
 impl crate::retry::Backoff for ExponentialBackoff {
-  async fn wait(&mut self, _retry: usize) {
-    tokio::time::sleep(Duration::from_secs(self.wait_for as u64)).await;
-    self.wait_for = self.wait_for.pow(self.exponent);
+  async fn wait(&mut self, retry: usize) {
+    let duration = std::cmp::min(self.max, self.start * 2_u32.pow(retry as u32));
+    tokio::time::sleep(Duration::from_secs(duration as u64)).await;
   }
 }
 
@@ -32,12 +21,6 @@ mod tests {
   use crate::retry::Retry;
   use std::time::Instant;
 
-  #[test]
-  #[should_panic(expected = "initial waiting time must be greater than 1")]
-  fn binary_initial_wait_time_must_be_greater_than_1() {
-    let _ = ExponentialBackoff::binary(0);
-  }
-
   #[tokio::test]
   async fn smoke() {
     let start = Instant::now();
@@ -45,7 +28,7 @@ mod tests {
 
     let result: Result<i32, &str> = Retry::new()
       .retries(3)
-      .backoff(ExponentialBackoff::binary(2))
+      .backoff(ExponentialBackoff { start: 1, max: 12 })
       .exec(|| {
         tries += 1;
 
