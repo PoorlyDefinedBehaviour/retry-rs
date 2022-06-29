e2d424873ba17b3d898fb2e1b3c556afd9ebe454
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2022-06-29 20:50:45 -0300

    [wip] feat: circuit breaker with retries and backoff

--- circuit_breaker/src/lib.rs
+++ circuit_breaker/src/lib.rs
@@ -1,54 +1,92 @@
 use std::{
+  cell::Cell,
   future::Future,
-  sync::atomic::{AtomicI8, Ordering},
+  sync::{
+    atomic::{AtomicI8, Ordering},
+    Arc,
+  },
 };
+
+use retry::Backoff;
 
 const CLOSED: i8 = 0;
 const OPEN: i8 = 1;
 const HALF_OPEN: i8 = 2;
 
-pub struct CircuitBreaker {
+struct CircuitBreakerInner {
   /// Is the circuit breaker closed, open or half open?
   state: AtomicI8,
+}
+
+pub struct CircuitBreaker {
+  /// The state that is shared between circuit breaker instances.
+  inner: Arc<CircuitBreakerInner>,
   /// The number of retries before considering a call as failed.
   retries: u32,
+  /// The backoff strategy.
+  backoff: Option<Arc<dyn Backoff>>,
 }
 
 impl CircuitBreaker {
   pub fn new() -> Self {
     Self {
-      state: AtomicI8::new(CLOSED),
+      inner: Arc::new(CircuitBreakerInner {
+        // Circuit starts closed.
+        state: AtomicI8::new(CLOSED),
+      }),
+      /// Will try to execute the closure passed to exec only once by default.
       retries: 1,
+      backoff: None,
     }
   }
 
-  pub fn retries(&mut self, n: u32) -> &mut Self {
+  /// Sets the number of retries for the next call.
+  pub fn with_retries(&self, n: u32) -> Self {
     assert!(n > 0, "retries must be greater than 0");
-    todo!()
+    Self {
+      inner: Arc::clone(&self.inner),
+      retries: n,
+      backoff: self.backoff.clone(),
+    }
   }
 
+  /// Sets backoff strategy for the next call.
+  pub fn with_backoff(&self, backoff: Arc<dyn Backoff>) -> Self {
+    Self {
+      inner: Arc::clone(&self.inner),
+      backoff: Some(backoff),
+      ..*self
+    }
+  }
+
+  /// Returns true when the circuit is open.
   pub fn is_open(&self) -> bool {
-    self.state.load(Ordering::Relaxed) == OPEN
+    self.inner.state.load(Ordering::Relaxed) == OPEN
   }
 
+  // Returns true when the circuit is half open.
   pub fn is_half_open(&self) -> bool {
-    self.state.load(Ordering::Relaxed) == HALF_OPEN
+    self.inner.state.load(Ordering::Relaxed) == HALF_OPEN
   }
 
+  /// Returns true when the circuit is closed.
   pub fn is_closed(&self) -> bool {
-    self.state.load(Ordering::Relaxed) == CLOSED
+    self.inner.state.load(Ordering::Relaxed) == CLOSED
   }
 
+  /// Opens the circuit.
   pub fn open(&self) {
-    self.state.store(OPEN, Ordering::Release);
+    self.inner.state.store(OPEN, Ordering::Release);
   }
 
+  /// Transitions the circuit to half open.
   pub fn half_open(&self) {
-    self.state.store(HALF_OPEN, Ordering::Release);
+    self.inner.state.store(HALF_OPEN, Ordering::Release);
   }
 
+  /// Closes the circuit.
   pub fn close(&self) {
-    self.state.store(CLOSED, Ordering::Release);
+    self.inner.state.store(CLOSED, Ordering::Release);
   }
 }
 
@@ -67,26 +105,49 @@ impl<T, E> From<Result<T, E>> for ExecResult<T, E> {
 }
 
 impl CircuitBreaker {
-  pub async fn exec<F, T, E, Fut, Out>(&self, f: F) -> Out
+  pub async fn exec<F, T, E, Fut, Out>(&self, mut f: F) -> Result<T, E>
   where
-    F: FnOnce() -> Fut,
+    F: FnMut() -> Fut,
     Fut: Future<Output = Out>,
     Out: Into<ExecResult<T, E>>,
   {
-    f().await
+    let mut retry = 0;
+    loop {
+      match f().await.into() {
+        ExecResult::Err(err) => {
+          if retry == self.retries - 1 {
+            return Err(err);
+          }
+        }
+        ExecResult::Ok(value) => return Ok(value),
+      }
+      retry += 1;
+    }
   }
 }
 
 #[cfg(test)]
 mod tests {
-  use retry::FullJitterExponentialBackoff;
+  use std::rc::Rc;
 
   use super::*;
+  use async_trait::async_trait;
+  use mockall::mock;
+  use proptest::prelude::*;
+  use tokio::runtime::Runtime;
+
+  mock! {
+      Backoff {}
+      #[async_trait]
+      impl retry::Backoff for Backoff {
+        async fn wait(&mut self, retry: usize);
+      }
+  }
 
   #[test]
   #[should_panic(expected = "retries must be greater than 0")]
   fn retries_must_be_greater_than_0() {
-    CircuitBreaker::new().retries(0);
+    CircuitBreaker::new().with_retries(0);
   }
 
   #[test]
@@ -118,18 +179,67 @@ mod tests {
     assert!(cb.is_closed());
   }
 
-  #[tokio::test]
-  async fn can_retry_before_considering_a_call_as_failed() {
-    let cb = CircuitBreaker::new();
+  proptest! {
+    #[test]
+    fn can_retry_before_considering_a_call_as_failed(num_retries:u16) {
+      prop_assume!(num_retries > 0);
 
-    let result = cb
-      .retries(3, FullJitterExponentialBackoff::recommended())
-      .exec_with(|| async {
-        // ...
-        Result::<i32, i32>::Ok(1)
-      })
-      .await;
+      Runtime::new().unwrap().block_on(async {
+        let cb = CircuitBreaker::new();
 
-    dbg!(&result);
+        let tries = Rc::new(Cell::new(0));
+
+        let result = cb
+          .with_retries(num_retries as u32)
+          .exec(|| async {
+            tries.set(tries.get() + 1);
+            if tries.get() == num_retries {
+              Ok(1)
+            } else {
+              Err(0)
+            }
+          })
+          .await;
+
+        assert_eq!(Ok(1), result);
+        assert_eq!(num_retries, tries.get());
+      });
+    }
+
+    #[test]
+    fn add_backoff_between_retries(num_retries: u16) {
+      prop_assume!(num_retries > 0);
+
+      Runtime::new().unwrap().block_on(async {
+        let cb = CircuitBreaker::new();
+
+        let tries = Rc::new(Cell::new(0_u16));
+        let times_backoff_got_called = Rc::new(Cell::new(0_u16));
+
+        let expect =        MockBackoff::new()
+        .expect_wait()
+        .times(num_retries as usize)
+        .withf_st(|retry_number| *retry_number == tries.get() as usize)
+        .returning_st(|_| times_backoff_got_called.set(times_backoff_got_called.get()+1));
+
+        let result = cb
+          .with_retries(num_retries as u32)
+    //       .with_backoff(
+    //  todo!()
+    //       )
+          .exec(|| async {
+            tries.set(tries.get() + 1);
+            if tries.get() == num_retries {
+              Ok(1)
+            } else {
+              Err(0)
+            }
+          })
+          .await;
+
+        assert_eq!(Ok(1), result);
+        assert_eq!(num_retries, tries.get());
+      });
+    }
   }
 }
